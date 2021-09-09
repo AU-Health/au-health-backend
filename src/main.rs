6b799b306868aec097849b49103a7f620fdb5feb
Matt Wilkinson <mattwilki17@gmail.com>
2021-09-09 10:02:52 -0400

    config

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,33 @@
 //! Developed by AU Capstone group.
 
 #![warn(missing_docs)]
-mod auth;
-mod gql;
+
+use std::{future::Future, time::Duration};
 
 use async_graphql::{
     http::{playground_source, GraphQLPlaygroundConfig},
     EmptyMutation, EmptySubscription, Request, Response, Schema,
 };
+use au_health_backend::{
+    configuration::{self, get_configuration},
+    gql::{GqlSchema, QueryRoot},
+};
 use axum::{
     extract::Extension,
     handler::get,
+    http::StatusCode,
     response::{Html, IntoResponse},
     AddExtensionLayer, Json, Router,
 };
-use gql::{GqlSchema, QueryRoot};
+use sqlx::postgres::PgPoolOptions;
 
 /// initalize GraphQL Playground UI for testing.
 async fn graphql_playground() -> impl IntoResponse {
     Html(playground_source(GraphQLPlaygroundConfig::new("/")))
+}
+
+async fn forbidden_response() -> impl IntoResponse {
+    StatusCode::FORBIDDEN
 }
 
 /// Processes GraphQL requests.
@@ -30,10 +39,23 @@ async fn graphql_handler(schema: Extension<GqlSchema>, req: Json<Request>) -> Js
 /// Entry point to server.
 #[tokio::main]
 async fn main() {
-    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+    let configuration = get_configuration().expect("Failed to load configuration");
+
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(Duration::from_secs(2))
+        .connect_with(configuration.database.with_db())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(connection_pool)
+        .finish();
 
     let router = Router::new()
-        .route("/", get(graphql_playground).post(graphql_handler))
+        .route(
+            &configuration.application.graphql_path,
+            get(graphql_playground).post(graphql_handler),
+        )
         .layer(AddExtensionLayer::new(schema));
 
     println!("Playground: http://localhost:3000");
